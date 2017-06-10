6d85a0e4767507b39549e11888e2814563cf37dc
Artur Grzes <grzesa@poczta.onet.pl>
2017-06-10 15:52:20 +0200

    2

--- js/tablice.js
+++ js/tablice.js
@@ -9,3 +9,15 @@ console.log(imiona);
 
 imiona.push('Gerald');
 console.log(imiona);
+
+imiona.pop();
+imiona.pop();
+
+console.log(imiona);
+
+console.log(imiona.unshift('Robert'));
+console.log(imiona);
+
+
+console.log(imiona.shift());
+console.log(imiona);
